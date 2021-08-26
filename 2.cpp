96066923c1a331d4ebe4d9b87eceaf763b12a774
BaitoeyKP <88471597+BaitoeyKP@users.noreply.github.com>
2021-08-26 12:02:30 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int x = 1;
+	printf("Enter number : ");
+	scanf("%d" , &n);
+	while(x <= n){
+		int y=1;
+		while(y <= n){
+			printf("*");
+			y++;
+		}
+		printf("\n");
+		x++;
+	}
+	return 0;
+}
